f27f86539b988bdeec4813a08ce79f749e4243b5
syed Devop <syeds.devops007@gmail.com>
2023-05-11 20:39:33 +0530

    Add rust

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -16,4 +16,11 @@ return {
       },
     },
   },
+
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "rust-analyzer" })
+    end,
+  },
 }
---
+++ lua/plugins/rust.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
+  {
+    "saecki/crates.nvim",
+    ft = { "rust", "toml" },
+    config = function(_, opts)
+      local crates = require("crates")
+      crates.setup(opts)
+    end,
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "crates" } }))
+    end,
+  },
+}
