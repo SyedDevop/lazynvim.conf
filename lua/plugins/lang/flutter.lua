6c1479e24c69b8479a81493b7b83b847ada11125
syed Devop <syeds.devops007@gmail.com>
2023-06-21 12:59:01 +0530

    feat: add flutter

---
+++ lua/plugins/lang/flutter.lua
@@ -0,0 +1,14 @@
+return {
+  "akinsho/flutter-tools.nvim",
+  lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "stevearc/dressing.nvim", -- optional for vim.ui.select
+  },
+  opts = function()
+    require("flutter-tools").setup({})
+  end,
+  keys = {
+    { "<leader>K", "<Cmd>lua vim.lsp.buf.hover()<CR>", desc = "Show hover" },
+  },
+}
