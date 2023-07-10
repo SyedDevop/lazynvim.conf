a7cff75c8e36f8dd7f2d8f0bf835d51591ac51d6
syed Devop <syeds.devops007@gmail.com>
2023-07-10 12:11:21 +0530

    refactor(rust): using lazy's lan.rust for rust config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,14 +10,16 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import any extras modules here
+
+    -- Language plugins
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
-    -- import/override with your plugins
+
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "plugins" },
   },
