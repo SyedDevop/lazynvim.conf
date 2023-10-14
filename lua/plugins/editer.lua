a78aa435821d59fce823f3d45509e01b7cd7b155
syed Devop <syeds.devops007@gmail.com>
2023-10-14 19:37:17 +0530

    refactor: removed bufdelete

--- lua/plugins/editer.lua
+++ lua/plugins/editer.lua
@@ -1,36 +1,4 @@
 return {
-
-  -- disable mini.bufremove
-  { "echasnovski/mini.bufremove", enabled = false },
-
-  -- use bdelete instead
-  {
-    "famiu/bufdelete.nvim",
-    -- stylua: ignore
-    config = function()
-      -- switches to Alpha dashboard when last buffer is closed
-      local alpha_on_empty = vim.api.nvim_create_augroup("alpha_on_empty", { clear = true })
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "BDeletePost*",
-        group = alpha_on_empty,
-        callback = function(event)
-          local fallback_name = vim.api.nvim_buf_get_name(event.buf)
-          local fallback_ft = vim.api.nvim_buf_get_option(event.buf, "filetype")
-          local fallback_on_empty = fallback_name == "" and fallback_ft == ""
-          if fallback_on_empty then
-            require("neo-tree").close_all()
-            vim.cmd("Dashboard")
-            vim.cmd(event.buf .. "bwipeout")
-          end
-        end,
-      })
-    end,
-    keys = {
-      { "<leader>bd", "<CMD>Bdelete<CR>", desc = "Delete Buffer" },
-      { "<leader>bD", "<CMD>Bdelete!<CR>", desc = "Delete Buffer (Force)" },
-    },
-  },
-
   -- customize file explorer
   {
     "nvim-neo-tree/neo-tree.nvim",
@@ -51,94 +19,31 @@ return {
     event = "BufReadPre",
   },
 
+  --   {
+  --     "nvimdev/dashboard-nvim",
+  --     opts = function(_, opts)
+  --       local logo = [[
+  --  █    ██ ▒███████▒ ▄▄▄       ██▓ ██▀███
+  --  ██  ▓██▒▒ ▒ ▒ ▄▀░▒████▄    ▓██▒▓██ ▒ ██▒
+  -- ▓██  ▒██░░ ▒ ▄▀▒░ ▒██  ▀█▄  ▒██▒▓██ ░▄█ ▒
+  -- ▓▓█  ░██░  ▄▀▒   ░░██▄▄▄▄██ ░██░▒██▀▀█▄
+  -- ▒▒█████▓ ▒███████▒ ▓█   ▓██▒░██░░██▓ ▒██▒
+  -- ░▒▓▒ ▒ ▒ ░▒▒ ▓░▒░▒ ▒▒   ▓▒█░░▓  ░ ▒▓ ░▒▓░
+  -- ░░▒░ ░ ░ ░░▒ ▒ ░ ▒  ▒   ▒▒ ░ ▒ ░  ░▒ ░ ▒░
+  --  ░░░ ░ ░ ░ ░ ░ ░ ░  ░   ▒    ▒ ░  ░░   ░
+  --    ░       ░ ░          ░  ░ ░     ░
+  --          ░
+  --       ]]
+  --       logo = string.rep("\n", 8) .. logo .. "\n\n"
+  --       opts.config.header = vim.split(logo, "\n")
+  --     end,
+  --   },
+
   -- git conflict
   {
     "akinsho/git-conflict.nvim",
     event = "BufReadPre",
     config = true,
-  },
-
-  -- add symbols-outline
-  {
-    "simrat39/symbols-outline.nvim",
-    cmd = "SymbolsOutline",
-    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
-    config = function()
-      local icons = require("lazyvim.config").icons
-      require("symbols-outline").setup({
-        symbols = {
-          File = { icon = icons.kinds.File, hl = "TSURI" },
-          Module = { icon = icons.kinds.Module, hl = "TSNamespace" },
-          Namespace = { icon = icons.kinds.Namespace, hl = "TSNamespace" },
-          Package = { icon = icons.kinds.Package, hl = "TSNamespace" },
-          Class = { icon = icons.kinds.Class, hl = "TSType" },
-          Method = { icon = icons.kinds.Method, hl = "TSMethod" },
-          Property = { icon = icons.kinds.Property, hl = "TSMethod" },
-          Field = { icon = icons.kinds.Field, hl = "TSField" },
-          Constructor = { icon = icons.kinds.Constructor, hl = "TSConstructor" },
-          Enum = { icon = icons.kinds.Enum, hl = "TSType" },
-          Interface = { icon = icons.kinds.Interface, hl = "TSType" },
-          Function = { icon = icons.kinds.Function, hl = "TSFunction" },
-          Variable = { icon = icons.kinds.Variable, hl = "TSConstant" },
-          Constant = { icon = icons.kinds.Constant, hl = "TSConstant" },
-          String = { icon = icons.kinds.String, hl = "TSString" },
-          Number = { icon = icons.kinds.Number, hl = "TSNumber" },
-          Boolean = { icon = icons.kinds.Boolean, hl = "TSBoolean" },
-          Array = { icon = icons.kinds.Array, hl = "TSConstant" },
-          Object = { icon = icons.kinds.Object, hl = "TSType" },
-          Key = { icon = icons.kinds.Key, hl = "TSType" },
-          Null = { icon = icons.kinds.Null, hl = "TSType" },
-          EnumMember = { icon = icons.kinds.EnumMember, hl = "TSField" },
-          Struct = { icon = icons.kinds.Struct, hl = "TSType" },
-          Event = { icon = icons.kinds.Event, hl = "TSType" },
-          Operator = { icon = icons.kinds.Operator, hl = "TSOperator" },
-          TypeParameter = { icon = icons.kinds.TypeParameter, hl = "TSParameter" },
-        },
-      })
-    end,
-  },
-
-  {
-    "akinsho/bufferline.nvim",
-    opts = {
-      options = {
-        numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
-        close_command = function(n)
-          require("bufdelete").bufdelete(n, true)
-        end, -- can be a string | function, see "Mouse actions"
-        right_mouse_command = function(n)
-          require("bufdelete").bufdelete(n, true)
-        end, -- can be a string | function, see "Mouse actions"
-        max_name_length = 30,
-        max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
-        show_buffer_icons = true,
-        show_buffer_close_icons = true,
-        show_close_icon = true,
-        show_tab_indicators = true,
-        separator_style = "slant", -- | "thick" | "thin" | { 'any', 'any' },
-        color_icons = true,
-        diagnostics = false,
-        highlights = {
-          buffer_selected = {
-            gui = "none",
-          },
-        },
-        offsets = {
-          {
-            filetype = "neo-tree",
-            text = "Neo-tree",
-            highlight = "Directory",
-            text_align = "left",
-          },
-          {
-            filetype = "Outline",
-            text = "Symbols Outline",
-            highlight = "TSType",
-            text_align = "left",
-          },
-        },
-      },
-    },
   },
 
   --which-key
