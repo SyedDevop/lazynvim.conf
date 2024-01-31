955e0f881cac23c456951197667ff369de650b5e
syed Devop <syeds.devops007@gmail.com>
2024-01-31 20:28:09 +0530

    remove: color

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -2,4 +2,6 @@ return {
   { "echasnovski/mini.pairs", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
+  { "ellisonleao/gruvbox.nvim", enabled = false },
 }
