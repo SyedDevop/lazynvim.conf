d26e971856ac5a5c73e80daed885f55fe54c4679
Syed <syeds.devops007@gmail.com>
2024-05-31 23:59:02 +0530

    remove: lsp name from lualine.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,43 +1,8 @@
-return {
+M = {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
   opts = function(_, opts)
-    local function lsp_name(msg)
-      msg = msg or "Inactive"
-      local buf_clients = vim.lsp.get_active_clients()
-      if next(buf_clients) == nil then
-        if type(msg) == "boolean" or #msg == 0 then
-          return "Inactive"
-        end
-        return msg
-      end
-      local buf_client_names = {}
-
-      for _, client in pairs(buf_clients) do
-        if client.name == "emmet_language_server" then
-          table.insert(buf_client_names, "emmet-ls")
-        else
-          table.insert(buf_client_names, client.name)
-        end
-      end
-
-      return table.concat(buf_client_names, ", ")
-    end
-    opts.options = {
-      theme = "auto",
-      globalstatus = true,
-      disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" }, winbar = { "dashboard" } },
-    }
     opts.sections = vim.tbl_deep_extend("force", opts.sections, {
-      lualine_y = {
-        {
-          lsp_name,
-          icon = " LSP:",
-          color = { gui = "none" },
-        },
-        { "progress", separator = " ", padding = { left = 1, right = 0 } },
-        { "location", padding = { left = 0, right = 1 } },
-      },
       lualine_z = {
         function()
           return "  " .. os.date("%r")
@@ -54,3 +19,5 @@ return {
     }
   end,
 }
+
+return M
