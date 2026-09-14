5adf2ec4f648196aa45fc881d9f0819de47758bd
SyedDevop <syeds.devops007@gmail.com>
2026-09-14 19:53:24 +0530

    clean up

--- lua/plugins/lang/python.lua
+++ lua/plugins/lang/python.lua
@@ -1,26 +1,8 @@
 return {
-  -- {
-  --   "nvimtools/none-ls.nvim",
-  --   optional = true,
-  --   opts = function(_, opts)
-  --     local nls = require("null-ls")
-  --     table.insert(opts.sources, nls.builtins.formatting.black)
-  --   end,
-  -- },
-
   {
     "mason-org/mason.nvim",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, { "black" })
     end,
   },
-  -- {
-  --   "linux-cultist/venv-selector.nvim",
-  --   dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim" },
-  --   event = "VeryLazy",
-  --   opts = function(_, opts)
-  --     opts.parents = 0
-  --     opts.enable_debug_output = true
-  --   end,
-  -- },
 }
